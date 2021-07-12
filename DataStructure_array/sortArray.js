73c757b2d4a67dcd05d126d44c628f99b8a60b96
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-07-12 23:31:57 +0530

    added: array practice programs

---
+++ DataStructure_array/sortArray.js
@@ -0,0 +1,23 @@
+/**
+ * Purpose Sort array and Find 2nd largest Number
+ * @author: Sanyukta Nirmal
+ * @since: 12/7/2021
+ */
+
+/**
+ * Sorting an array
+ */
+
+let array=new Array();
+for(let i=0;i<10;i++){
+    let randomNumber=Math.floor(Math.random()*900+100);
+    array.push(randomNumber);
+}
+console.log("Array before sorting");
+console.log(array);
+array.sort();
+console.log("Array after sorting");
+console.log(array);
+console.log("second Largest Number:"+array[8]);
+console.log("second smallest number:"+array[1]);
+
